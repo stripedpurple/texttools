225a22904d1b80fd7bb5d6336c56d7a4b5c3ad18
Austin Barrett <virus.cmd@gmail.com>
2019-08-17 11:50:40 -0400

    WIP: gh-pages deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,11 @@
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/texttools/'
+  }
+} : {}
 
 export default {
+  ...routerBase,
   mode: 'universal',
   /*
   ** Headers of the page
